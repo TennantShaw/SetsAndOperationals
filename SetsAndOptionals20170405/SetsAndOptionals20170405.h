02262c5a172f86cc4c4f991443b9a7cbb4de2e5f
Tennant Shaw <tennantshaw@Macs-MacBook-Pro.local>
2017-04-06 19:23:30 -0400

    Initial Commit

---
+++ SetsAndOptionals20170405/SetsAndOptionals20170405.h
@@ -0,0 +1,19 @@
+//
+//  SetsAndOptionals20170405.h
+//  SetsAndOptionals20170405
+//
+//  Created by Tennant Shaw on 4/6/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SetsAndOptionals20170405.
+FOUNDATION_EXPORT double SetsAndOptionals20170405VersionNumber;
+
+//! Project version string for SetsAndOptionals20170405.
+FOUNDATION_EXPORT const unsigned char SetsAndOptionals20170405VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SetsAndOptionals20170405/PublicHeader.h>
+
+
