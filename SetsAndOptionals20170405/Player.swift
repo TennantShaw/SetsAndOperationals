3ed11be9ef3fe6d8d3c59622d67536ec28eebf88
Tennant Shaw <tennantshaw@Macs-MBP.lan>
2017-04-13 08:42:03 -0400

    updating swift file

--- SetsAndOptionals20170405/Player.swift
+++ SetsAndOptionals20170405/Player.swift
@@ -7,3 +7,88 @@
 //
 
 import Foundation
+
+enum Symbol {
+    case x
+    case o
+}
+
+typealias Move1 = (location: Int, symbol: Symbol)
+
+struct Location : Equatable {
+    var x: Int
+    var y: Int
+    
+    public static func == (lhs: Location, rhs: Location) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
+    }
+}
+
+struct Player : Equatable {
+    var name: String
+    let symbol: Symbol
+    
+    public static func == (lhs: Player, rhs: Player) -> Bool {
+        return lhs.name == rhs.name && lhs.symbol == rhs.symbol
+    }
+}
+
+struct Game : Equatable {
+    let playerOne: Player
+    let playerTwo: Player
+    var playerMove: [Move] = []
+    
+    public static func == (lhs: Game, rhs: Game) -> Bool {
+        return lhs.playerOne == rhs.playerOne && lhs.playerTwo == rhs.playerTwo
+    }
+}
+
+struct Move : Equatable {
+    let location: (Int, Int)
+    let symbol: Symbol
+    
+    public static func == (lhs: Move, rhs: Move) -> Bool {
+        return lhs.location == rhs.location && lhs.symbol == rhs.symbol
+    }
+}
+
+
+// Create Player func
+
+func createPlayer(name: String, symbol: Symbol) -> Player? {
+    if name.isEmpty {
+        return nil
+    } else {
+        return Player(name: name, symbol: symbol)
+    }
+}
+
+var firstPlayer = createPlayer(name: "Tennant", symbol: .x)
+var secondPlayer = createPlayer(name: "Shannon", symbol: .o)
+
+// Create Game func
+
+func createGame(player: Player, secondPlayer: Player) -> Game? {
+    if player.symbol == secondPlayer.symbol {
+        return nil
+    } else {
+        return Game(playerOne: player, playerTwo: secondPlayer, playerMove: [])
+    }
+}
+
+var newGame = createGame(player: firstPlayer!, secondPlayer: secondPlayer!)
+
+// Create a play func
+
+func playerTurn (player: Player, move: Location, game: Game) -> Game? {
+    return nil
+}
+
+
+
+
+
+
+
+
+
--- SetsAndOptionals20170405Tests/SetsAndOptionals20170405Tests.swift
+++ SetsAndOptionals20170405Tests/SetsAndOptionals20170405Tests.swift
@@ -11,26 +11,79 @@ import XCTest
 
 class SetsAndOptionals20170405Tests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // Write a type, player with two properties, name and symbol. There are two valid symbols: x and o.
+    // Write a function, createPLayer, which accepts a name and a symbol, returns a player with the provided values if the provided name is non-empty…
+    func testCreatePlayerWithNonemptyName() {
+        let result = createPlayer(name: "Tennant", symbol: .x)
+        let expected = Player(name: "Tennant", symbol: .x)
+        XCTAssertEqual(result, expected)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    //  …and returns nothing if the name was empty.
+    func testCreatePlayerWithEmptyName() {
+        let result = createPlayer(name: "", symbol: .x)
+        let expected: Player? = nil
+        XCTAssertEqual(result, expected)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    // Write a type game which holds two players with differing symbols.
+    func testCreateGameWithValidPlayers() {
+        let playerOne = Player(name: "Tennant", symbol: .x)
+        let playerTwo = Player(name: "Shannon", symbol: .o)
+        
+        let result = createGame(player: playerOne, secondPlayer: playerTwo)
+        let expected: Game? = Game(playerOne: playerOne, playerTwo: playerTwo, playerMove: [])
+        XCTAssertEqual(result, expected)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    // Write a type game which holds two players with differing symbols.
+    func testCreateGameWithInvalidPlayers() {
+        let playerOne = Player(name: "Tennant", symbol: .x)
+        let playerTwo = Player(name: "Shannon", symbol: .x)
+        
+        let result = createGame(player: playerOne, secondPlayer: playerTwo)
+        let expected: Game? = nil
+        XCTAssertEqual(result, expected)
     }
     
+    // Add a property, moves, to the type Game. A move should exist of a player and a location within a 3 x 3 grid
+    // write a function, play, which accepts a player, a location, and a game and returns a new game with a move for the provided player at the provided location added to the moves property of the game.
+    func testPlay() {
+        
+        //    0 | 1 | 2
+        //   -----------
+        //    3 | 4 | 5
+        //   -----------
+        //    6 | 7 | 8
+        let liz = Player(name: "Tennant", symbol: .x)
+        let bart = Player(name: "Shannon", symbol: .o)
+        let game = Game(liz, bart, moves: [])
+        
+        let result = play(player: liz, location: 4, game: game)
+        let expected = Game(liz, bart, moves: [(playerSymbol: .x, location: 4)])
+        XCTAssertEqual(result, expected)
+        
+    }
+    
+    // write a function, symbol(at location: Location, in game: Game )
+    func testSymbolAtOccupiedLocation() {
+        let liz = Player(name: "Tennant", symbol: .x)
+        let bart = Player(name: "Shannon", symbol: .o)
+        let game = Game(liz, bart, moves: [(playerSymbol: .x, location: 4), (playerSymbol: .o, location: 2)])
+        let result = symbol(at: 4, in: game)
+        let expected: Symbol = .x
+        
+        XCTAssertEqual(result, expected)
+    }
+    
+    func testSymbolAtUnoccupiedLocation() {
+        let liz = Player(name: "Tennant", symbol: .x)
+        let bart = Player(name: "Shannon", symbol: .o)
+        let game = Game(liz, bart, moves: [(playerSymbol: .x, location: 4), (playerSymbol: .o, location: 2)])
+        let result = symbol(at: 0, in: game)
+        let expected: Symbol? = nil
+        
+        XCTAssertEqual(result, expected)
+    }
 }
